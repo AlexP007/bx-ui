8a3cdd64fbac9ea179bbb83c2ddc1ee2a2c39c6a
AlexP <alex.p.panteleev@gmail.com>
2020-03-26 18:49:52 +0300

    deal with input counter

--- assets/dev/js/src/component/InputCounter.js
+++ assets/dev/js/src/component/InputCounter.js
@@ -1,12 +1,35 @@
 import Counter from "./Counter";
 
 export default class InputCounter extends Counter {
+    addEventListeners() {
+        super.addEventListeners();
+        const onChange = BX.proxy(this.onChange, this);
+
+        BX.bind(this.getElement(), 'change', onChange);
+    };
+
+    onChange(e) {
+        let val = 0;
+        if (e.target.value > this.max) {
+            val = this.max;
+        } else if (e.target.value < this.min) {
+            val = this.min;
+        }
+
+        this.counter = +val;
+        this.getElement().value = +val;
+    };
+
     increment(step) {
         if (this.max && this.counter >= this.max) {
             return;
         }
         step = step || 1;
         this.counter += step;
+
+        if (this.counter > this.max) {
+            this.counter = this.max;
+        }
 
         this.getElement().value = this.counter;
     };
@@ -17,6 +40,10 @@ export default class InputCounter extends Counter {
         }
         step = step || 1;
         this.counter -= step;
+
+        if (this.counter < this.min) {
+            this.counter = this.min;
+        }
 
         this.getElement().value = this.counter;
     };
